ff2ade67a50bf67f4fdb829b72bd803898998495
bi3mer <colan@comcast.net>
2024-06-11 10:43:12 -0500

    working on a tutorial scene. It isn't complete so there is no update to bundle.js`

--- src/Scenes/index.ts
+++ src/Scenes/index.ts
@@ -2,10 +2,12 @@ import { Game } from "./Game";
 import { PlayerLost } from "./PlayerLost";
 import { PlayerWon } from "./PlayerWon";
 import { StartMenu } from "./StartMenu";
+import { Tutorial } from "./Tutorial";
 
 export const Scene = {
   Game,
   PlayerLost,
   PlayerWon,
-  StartMenu
+  StartMenu,
+  Tutorial,
 };
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,16 @@ const startScene = new Scene.StartMenu();
 const gameScene = new Scene.Game();
 const playerLostScene = new Scene.PlayerLost();
 const playerWonScene = new Scene.PlayerWon();
+const tutorialScene = new Scene.Tutorial();
 
 const startIndex = engine.addScene(startScene);
 const gameIndex = engine.addScene(gameScene);
 const lostIndex = engine.addScene(playerLostScene);
 const wonIndex = engine.addScene(playerWonScene);
+const tutorialIndex = engine.addScene(tutorialScene)
 
 startScene.sceneIndex = gameIndex;
+startScene.sceneIndex = tutorialIndex;
 
 gameScene.playerLostIndex = lostIndex;
 gameScene.playerWonIndex = wonIndex;
