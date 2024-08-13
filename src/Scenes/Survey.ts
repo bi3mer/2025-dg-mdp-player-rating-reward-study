55433b0e0914f81523532e5e67cdfcd87a0ff882
bi3mer <colan@comcast.net>
2024-08-13 10:34:04 -0400

    basic survey implemented in terms of looks, but there is no submission button yet

--- src/Scenes/Survey.ts
+++ src/Scenes/Survey.ts
@@ -1,4 +1,6 @@
 
+import { QUESTIONS } from "../questions";
+import { fischerYatesShuffle } from "../Random";
 import { Engine, Scene } from "../WorldEngine";
 
 export class Survey extends Scene {
@@ -8,7 +10,56 @@ export class Survey extends Scene {
   }
 
   public onEnter(engine: Engine): void {
-    console.log('survey!');
+    // double shuffle because why not
+    fischerYatesShuffle(QUESTIONS);
+    fischerYatesShuffle(QUESTIONS);
+
+    // get where we are going to place the survey
+    const questionnaire = document.getElementById("questionnaire");
+
+    // populate HTML with the questions
+    let i = 0;
+    for (; i < QUESTIONS.length; ++i) {
+      const q = QUESTIONS[i];
+      const id = q.split(' ').join('_');
+      
+      const formElement = `<fieldset>
+      <label for="${id}"><b>${q}</b></label>
+        <br/>
+        <br/>
+        <table>
+          <tr>
+            <td></td>
+            <td>-3</td>
+            <td>-2</td>
+            <td>-1</td>
+            <td>&#20;0</td>
+            <td>&#20;1</td>
+            <td>&#20;1</td>
+            <td>&#20;2</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Strongly disagree</td>
+            <td><input type="radio" name="${id}" value="-3"/></td>
+            <td><input type="radio" name="${id}" value="-2"/></td>
+            <td><input type="radio" name="${id}" value="-1"/></td>
+            <td><input type="radio" name="${id}" value="0"/></td>
+            <td><input type="radio" name="${id}" value="1"/></td>
+            <td><input type="radio" name="${id}" value="2"/></td>
+            <td><input type="radio" name="${id}" value="3"/></td>
+            <td>Strongly agree</td>
+          </tr>
+        </table>
+      </fieldset>
+      <br/>`;
+
+      questionnaire!.innerHTML += formElement;
+    }
+
+    // hide game and make the survey visible
+    document.getElementById('canvas')!.style.display = "none";
+    document.getElementById('survey')!.style.display = "block";
   }
 
   public onExit(engine: Engine): void { }
