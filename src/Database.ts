87cef80f6eb12f0964437b5ce6ed3a8430c2def2
bi3mer <colan@comcast.net>
2025-02-19 15:24:42 -0500

    data logged to db now includes demogrpahic data

--- src/Database.ts
+++ src/Database.ts
@@ -49,6 +49,12 @@ export class DB {
   }
 
   public static submitSurvey(survey: { [key: string]: any }) {
+    const keys = Object.keys(Global.demographicSurveyData);
+    for (let i = 0; i < keys.length; ++i) {
+      const k = keys[i];
+      survey[k] = Global.demographicSurveyData[k];
+    }
+
     // only log to the server if this is a real participant
     if (Global.playerID === "null") {
       console.log("Data not submitted:", survey);
