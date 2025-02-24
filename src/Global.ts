1be136eac95a5992c648a769f6295c92b794b961
bi3mer <colan@comcast.net>
2025-02-24 13:25:56 -0500

    updated to major version

--- src/Global.ts
+++ src/Global.ts
@@ -9,7 +9,7 @@ export class Global {
   public static playerGaveUp = false;
   public static gamesPlayed = 0;
   public static director = "-1";
-  public static version = "0.0.3";
+  public static version = "1.0.0";
 
   // Tutorial data
   public static tutorialDiedToEnemy = 0;
